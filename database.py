4b21572627afeaf50584b97cfcb796da7d65414d
rifandyzv <rifandy2@gmail.com>
2022-06-08 20:45:01 +0700

    add database connection

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from secret import DB_UNAME, DB_IP, DB_PW
 # engine = sqlalchemy.create_engine("postgresql://admin:admin@localhost:5432/user")
 # engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@db-mariadb:3306/user")
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root@localhost:3306/user")
+# engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root@localhost:3306/user")
+connectionString = "mysql://{uname}:{pw}@{ip}:3306/user".format(uname=DB_UNAME, pw=DB_PW, ip=DB_IP)
+engine = sqlalchemy.create_engine(connectionString)
 # engine = sqlalchemy.create_engine("postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo")
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ secret.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DB_UNAME = os.getenv("DB_UNAME")
+DB_PW = os.getenv("DB_PW")
+DB_IP = os.getenv("DB_IP")
+
+connectionString = "mysql://{uname}:{pw}@{ip}:3306/user".format(uname=DB_UNAME, pw=DB_PW, ip=DB_IP)
+
+print(connectionString)
